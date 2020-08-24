5f02039e4d69434f8e2f98183cbbb7151b961786
TBXark <tbxark@outlook.com>
2020-08-24 17:14:36 +0800

    TelegramAPI  add json request

--- AutoGenerated/main.swift
+++ AutoGenerated/main.swift
@@ -75,8 +75,8 @@ class ParsingController {
                 }
             }
         }
-        return (type: typeFileBuilder(content: telegramModel),
-                method: methodFileBuilder(content: telegramRequest))
+        return (type: typeFileBuilder(content: telegramModel).replacingOccurrences(of: "&#39;", with: "’"),
+                method: methodFileBuilder(content: telegramRequest).replacingOccurrences(of: "&#39;", with: "’"))
     }
 
 }
@@ -346,10 +346,49 @@ extension ParsingController {
 
         extension TelegramAPI {
 
+          
             /// Telegram Request wrapper
+            /// Authorizing your bot
+            ///
+            /// Each bot is given a unique authentication token when it is created. The token looks something like 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11, but we'll use simply <token> in this document instead. You can learn about obtaining tokens and generating new ones in this document.
+            ///
+            /// Making requests
+            ///
+            /// All queries to the Telegram Bot API must be served over HTTPS and need to be presented in this form: https://api.telegram.org/bot<token>/METHOD_NAME. Like this for example:
+            ///
+            /// https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe
+            /// We support GET and POST HTTP methods. We support four ways of passing parameters in Bot API requests:
+            ///
+            /// URL query string
+            /// application/x-www-form-urlencoded
+            /// application/json (except for uploading files)
+            /// multipart/form-data (use to upload files)
+            /// The response contains a JSON object, which always has a Boolean field 'ok' and may have an optional String field 'description' with a human-readable description of the result. If 'ok' equals true, the request was successful and the result of the query can be found in the 'result' field. In case of an unsuccessful request, 'ok' equals false and the error is explained in the 'description'. An Integer 'error_code' field is also returned, but its contents are subject to change in the future. Some errors may also have an optional field 'parameters' of the type ResponseParameters, which can help to automatically handle the error.
+            ///
+            /// All methods in the Bot API are case-insensitive.
+            /// All queries must be made using UTF-8.
+            /// Making requests when getting updates
+            /// If you're using webhooks, you can perform a request to the Bot API while sending an answer to the webhook. Use either application/json or application/x-www-form-urlencoded or multipart/form-data response content type for passing parameters. Specify the method to be invoked in the method parameter of the request. It's not possible to know that such a request was successful or get its result.
+            ///
+            /// Please see our FAQ for examples.
             public struct Request {
                 public let method: String
                 public let body: [String: Any]
+                
+                public func jsonRequest(token: String) throws -> URLRequest {
+                    let urlRaw = "https://api.telegram.org/bot\\(token)/\\(method)"
+                    guard let url = URL(string: urlRaw) else {
+                        throw NSError.init(domain: "com.tbxark.TelegramBotAPI",
+                                           code: -1,
+                                           userInfo: [NSLocalizedFailureReasonErrorKey: "URL(\\(urlRaw) is illegal"])
+                    }
+                    var req = URLRequest(url: url)
+                    req.httpMethod = "POST"
+                    req.addValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+                    req.httpBody = try AnyEncodable.encode(body)
+                    return req
+                }
+                
             }
 
             /// AnyEncodable
