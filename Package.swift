3c7ca98f6a8cc7686041920b508ed12d8325d42c
TBXark <tbxark@outlook.com>
2021-05-31 13:14:07 +0800

    Update API 2021.05.31

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Either",
+    name: "TelegramBotAPI",
     products: [
         .executable(name: "Generate", targets: ["Generate"]),
         .library(name: "TelegramBotAPI", targets: ["TelegramBotAPI"])
